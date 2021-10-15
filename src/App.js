173d644b41bba91d1f54f2575a510c41c2b03b59
ritu <ritumkapadia@gmail.com>
2021-10-15 17:37:40 +0530

    Added meaning by emoticon functionality

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ class App extends React.Component {
     super();
 
     let eData = new Map([
-      ["128512", ["smiling,smile,happy"]],
-      ["128514", ["laugh,laughing,cry laughing"]],
-      ["128518", ["naughty smile"]],
-      ["128520", ["devil,red devil"]],
-      ["128521", ["wynk"]],
-      ["128523", ["tasty,foodie"]],
-      ["128525", ["love eye,heart eye,two hearts eye"]],
-      ["128536", ["kiss,flying kiss,love kiss"]],
-      ["128147", ["love, heart"]],
-      ["128545", ["angry"]],
-      ["128532", ["sad"]]
+      ["🤩", ["Star-Struck"]],
+      ["😘", ["Face Blowing a Kiss"]],
+      ["🤪", ["Zany Face"]],
+      ["🤑", ["Money-Mouth Face"]],
+      ["😃", ["Grinning Face with Big Eyes"]],
+      ["😁", ["Beaming Face with Smiling Eyes"]],
+      ["😅", ["Grinning Face with Sweat"]],
+      ["🙃", ["Upside-Down Face"]],
+      ["😉", ["Winking Face"]],
+      ["😍", ["Smiling Face with Hearts"]],
+      ["😜", ["Face with Tongue"]],
+      ["🤫", ["Shushing Face"]],
+      ["😝", ["Squinting Face with Tongue"]],
+      ["🤗", ["Hugging Face"]],
+      ["🥱", ["Yawning Face"]],
+      ["🤔", ["Thinking Face"]],
+      ["🤐", ["Zipper-Mouth Face"]],
+      ["😐", ["Neutral Face"]],
+      ["😑", ["Expressionless Face"]],
+      ["😏", ["Smirking Face"]],
     ]);
 
+   
     this.state = {
       emoji: '',
       emojiData: eData
@@ -29,36 +39,35 @@ class App extends React.Component {
   }
 
   readInput(e) {
+    
     let val = e.target.value;
-    console.log(val);
-    document.getElementById('inputValue').innerHTML = val ;
-    this.state.emojiData.forEach((element, index) => {
-      if(element[0].split(",").includes(val.toLowerCase()))
-      {
-        this.setState({
-          emoji: index
-        });
-        console.log(this.state.emoji);
-        document.getElementById('ans').innerHTML = '&#'+index ;
-      }
-      // else
-      // {
-      //   document.getElementById('ans').innerHTML = 'Not found' ;
-      // }
-    });
+    let res = "";
+
+    if(val != "")
+      res = this.state.emojiData.get(val) ? this.state.emojiData.get(val) : "Emoji meaning not found";
+    else
+      res = "";
+      
+    document.getElementById('ans').innerHTML =  res;
+
   }
 
   writeEmoji(e) {
     document.getElementById('ans').innerHTML = e.target.id;
   }
 
+
   render(){
 
     let list = [];
+    let spanList = [];
+    let i = 0;
 
     this.state.emojiData.forEach((element, index) => {
       let str = '&#'+index+';';
       list.push(<p>{str}</p>);
+      if(i++ < 10)
+        spanList.push(<span id={element[0]} onClick={this.writeEmoji}>{index}</span>);        
     })
 
     return (
@@ -68,19 +77,16 @@ class App extends React.Component {
         <div className="container">
           <h2>Emoji Interpreter</h2>
           <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search Emoji by Name e.g., Sad, Angry" onChange={this.readInput.bind(this)} />
+            <input type="text" className="form-control" placeholder="Search Emoji meaning by emoticon e.g., 😍" onChange={this.readInput.bind(this)} />
           </div>
           <p id="inputValue"></p>
           <h2 id="ans"></h2>
         </div>
 
         <div className="container">
-          <span id="Happy" onClick={this.writeEmoji}>&#128512;</span>
-          <span id="Laugh" onClick={this.writeEmoji}>&#128514;</span>
-          <span id="Naughty" onClick={this.writeEmoji}>&#128518;</span>
-          <span id="Devil" onClick={this.writeEmoji}>&#128520;</span>
-          <span id="Wynk" onClick={this.writeEmoji}>&#128521;</span>
+        {spanList}
         </div>
+        <p className="mt-1">Click on any emoji to know its meaning</p>
       </div>
     );
   }
